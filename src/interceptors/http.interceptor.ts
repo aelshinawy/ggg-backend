2b5bd9001980353fe415e0eb6fb5ef6687fbdbf8
Ahmed El-Shinawy <aelshinawy92@gmail.com>
2023-04-10 05:39:13 +0200

    Placeholder interceptor code

--- src/interceptors/http.interceptor.ts
+++ src/interceptors/http.interceptor.ts
@@ -1,10 +1,20 @@
 import { HttpService } from '@nestjs/axios';
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 export class HttpServiceInterceptor implements NestInterceptor {
   constructor(private httpService: HttpService) {}
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        throw new Error("Method not implemented.");
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const ctx = context.switchToHttp();
+    const token = ctx.getRequest().headers['authorization'];
+
+    if (token) {
+      this.httpService.axiosRef.defaults.headers.common['authorization'] =
+        token;
     }
+    return next.handle().pipe();
+  }
 }
